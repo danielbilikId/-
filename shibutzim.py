0c332f260b547190eccc022921a1a6f03f51729f
daniel bilik <danielbilik2003@gmail.com>
2024-04-21 17:12:51 +0300

    remove comments

--- shibutzim.py
+++ shibutzim.py
@@ -1,7 +1,9 @@
 from allocate_duties import allocate_duties
+from gSheetsAPI import update_google_sheet
 
 people_file = 'people.xlsx'  
-weekdays_dates = ['2024-12-12', '2024-12-13', '2024-12-14', '2024-12-18']
-weekends_dates = ['2024-12-22', '2024-12-21']
+weekdays_dates = ['2024-08-08', '2024-08-13', '2024-08-14', '2024-08-18']
+weekends_dates = ['2024-08-22', '2024-08-21']
 
 df = allocate_duties(people_file, weekdays_dates, weekends_dates)
+update_google_sheet(df)
