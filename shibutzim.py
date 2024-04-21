796a2c607194c6d051a6f6b7cba9b0173b4ee7aa
daniel bilik <danielbilik2003@gmail.com>
2024-04-21 17:46:28 +0300

    remove comments

--- shibutzim.py
+++ shibutzim.py
@@ -1,8 +1,7 @@
 from allocate_duties import allocate_duties
-# Example usage
-people_file = 'people.xlsx'  # Path to the Excel file containing the list of people
-weekdays_dates = ['2024-09-09', '2024-09-13', '2024-09-14', '2024-09-18']
-weekends_dates = ['2024-09-22', '2024-09-21']
 
-# Allocate duties and display the result
+people_file = 'people.xlsx'  
+weekdays_dates = ['2024-12-12', '2024-12-13', '2024-12-14', '2024-12-18']
+weekends_dates = ['2024-12-22', '2024-12-21']
+
 df = allocate_duties(people_file, weekdays_dates, weekends_dates)
